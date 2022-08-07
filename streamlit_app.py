b94f64a014600258f7106388267d45aa7e5ae9ea
Aarya <aaryamittapalli@gmail.com>
2022-08-07 19:19:39 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import pandas as pd
 
-left_column, middle_column, right_column = st.columns(3)
+left_column, right_column = st.columns(2)
 # You can use a column just like st.sidebar:
 left_column.button('Press me!')
 
 df = pd.read_csv('my_data.csv')
-middle_column.line_chart(df)
+left_column.line_chart(df)
 
 # Or even better, call Streamlit functions inside a "with" block:
 with right_column:
