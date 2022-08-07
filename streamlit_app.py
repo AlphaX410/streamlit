01cf50d387483909274807c6fde2f687f04eddfb
Aarya <aaryamittapalli@gmail.com>
2022-08-07 19:13:21 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,9 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
+x = st.slider('x')  # 👈 this is a widget
+st.write(x, 'squared is', x * x)
+
 map_data = pd.DataFrame(
     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
     columns=['lat', 'lon'])
