dd453a9b8f8f0772d53fa65cf954913ae39c81fb
Aarya <aaryamittapalli@gmail.com>
2022-08-07 15:28:44 -0400

    streamlit example

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit as st
+import pandas as pd
 
 st.write("""
 # Sales Model
 Below are our sales predictions
 for this customer.
 """)
+df = pd.read_csv("my_data.csv")
+st.line_chart(df)
