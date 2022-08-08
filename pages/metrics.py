5a2811b5b321f504f276d7e2a30ae56784bb0f25
Aarya <aaryamittapalli@gmail.com>
2022-08-07 20:25:11 -0400

    Update metrics.py

--- pages/metrics.py
+++ pages/metrics.py
@@ -2,3 +2,8 @@ import streamlit as st
 
 st.markdown("# Metrics")
 st.sidebar.markdown("# Metrics")
+
+col1, col2, col3 = st.columns(3)
+col1.metric("Temperature", "70 °F", "1.2 °F")
+col2.metric("Wind", "9 mph", "-8%")
+col3.metric("Humidity", "86%", "4%")
